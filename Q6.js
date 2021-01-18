1433ddc74768686fc61743776bf6f338a2e0eebf
TomerNissim <tomernis@gmail.com>
2021-01-18 16:20:42 +0200

    Q6 complete

---
+++ Q6.js
@@ -0,0 +1,17 @@
+let mark1 = 80;
+let mark2 = 77;
+let mark3 = 88;
+let mark4 = 95;
+let mark5 = 68;
+let averageMarks = (mark1 + mark2 + mark3 + mark4 + mark5)/5;
+console.log("the avrage marks is" + averageMarks )
+if(averageMarks < 60)
+    console.log("The grade is F")
+else if(averageMarks < 70)
+console.log("The grade is D")    
+else if(averageMarks < 80)
+console.log("The grade is C")  
+else if(averageMarks < 90)
+console.log("The grade is B")  
+else 
+    console.log("The grade is A")  
