81c499d3eefe8c0a1f043f8603fa888dba13aefe
TomerNissim <tomernis@gmail.com>
2021-01-18 15:12:56 +0200

    Q1 complete

---
+++ Q1.js
@@ -0,0 +1,12 @@
+"use strict"
+let num1 = Number(prompt("please enter first number"));
+let num2 = Number(prompt("please enter second number"));
+
+if (num1 > num2){
+    alert( "the larger number is " + num1);
+}
+else {
+    alert( "the larger number is " + num2);
+}
+
+
