97d57f9259c5da6942604aa52afb47d7384e9a1b
TomerNissim <tomernis@gmail.com>
2021-01-18 18:55:42 +0200

    Q10 complete

---
+++ Q10.js
@@ -0,0 +1,14 @@
+
+let pyramidString = " the string is";
+for(let i = 1; i<=6; i++){
+    for(let j = 1; j < i; j++){
+        pyramidString = pyramidString + "*" ;
+    }
+    console.log(pyramidString);
+    pyramidString = " ";
+}
+
+
+
+
+
