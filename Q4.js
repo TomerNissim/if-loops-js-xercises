ddad448e83e1edf1b4323ea563e71200817e9c39
TomerNissim <tomernis@gmail.com>
2021-01-18 16:04:54 +0200

    Q4 complete

---
+++ Q4.js
@@ -0,0 +1,8 @@
+let listOfNumbers = [-5,-2,-6,0,-1];
+let biggest = listOfNumbers[4];
+for(let i = 0; i<= 3; i++){
+    if(listOfNumbers[i] > biggest)
+    biggest = listOfNumbers[i];
+
+}
+alert(biggest)
